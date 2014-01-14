6d5386917b9df813533b597385f9bc0a0aa0eda8
baigewd <baigewd@163.com>
2014-01-14 18:49:14 +0800

    1月14
    修改侧边栏   导航a 背景

--- js/indexbanner.js
+++ js/indexbanner.js
@@ -2,12 +2,12 @@ $(function(){
 	
 	//在线客服
 	$('.boxgrid.slideright').mouseover(function(){
-		$(".cover", this).stop().animate({width:'312px'},{queue:false,duration:300});
-		$('.boxgrid.slideright').animate({width:'312px'},{queue:false,duration:300});
+		$(".cover", this).stop().animate({width:'198px'},{queue:false,duration:300});
+		$('.boxgrid.slideright').animate({width:'198px'},{queue:false,duration:300});
 	});
 	$('.boxgrid.slideright').mouseout(function(){
-		$(".cover", this).stop().animate({width:'45px'},{queue:false,duration:300});
-		$('.boxgrid.slideright').animate({width:'45px'},{queue:false,duration:300});
+		$(".cover", this).stop().animate({width:'23px'},{queue:false,duration:300});
+		$('.boxgrid.slideright').animate({width:'23px'},{queue:false,duration:300});
 	});
 	$(window).scroll(function (){
 		var offsetTop = $(window).scrollTop() + 12 +"px";
