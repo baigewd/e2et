ea8519cee2e64778799b91de27fe89d4e1bb6c4a
baigewd <baigewd@163.com>
2014-01-15 10:44:30 +0800

    客服
    缩小

--- js/zxkf.js
+++ js/zxkf.js
@@ -1,14 +1,17 @@
 $(function(){
-$('.boxgrid.slideright').mouseover(function(){
-		$(".cover", this).stop().animate({width:'312px'},{queue:false,duration:300});
-		$('.boxgrid.slideright').animate({width:'312px'},{queue:false,duration:300});
+	
+	//在线客服
+	$('.boxgrid.slideright').mouseover(function(){
+		$(".cover", this).stop().animate({width:'198px'},{queue:false,duration:300});
+		$('.boxgrid.slideright').animate({width:'198px'},{queue:false,duration:300});
 	});
 	$('.boxgrid.slideright').mouseout(function(){
-		$(".cover", this).stop().animate({width:'45px'},{queue:false,duration:300});
-		$('.boxgrid.slideright').animate({width:'45px'},{queue:false,duration:300});
+		$(".cover", this).stop().animate({width:'23px'},{queue:false,duration:300});
+		$('.boxgrid.slideright').animate({width:'23px'},{queue:false,duration:300});
 	});
 	$(window).scroll(function (){
 		var offsetTop = $(window).scrollTop() + 12 +"px";
 		$("#Float").animate({top : offsetTop },{ queue:false , duration:500 });
 	}); 
+
 	});
